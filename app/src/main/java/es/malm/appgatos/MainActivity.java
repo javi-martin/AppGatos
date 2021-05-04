c78c0fccc43e30bc0a63e1fe201b22dd80aa6a5a
ylak963 <pepitogrilloreturn@gmail.com>
2021-05-04 10:04:12 +0200

    V0

---
+++ app/src/main/java/es/malm/appgatos/MainActivity.java
@@ -0,0 +1,27 @@
+package es.malm.appgatos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity
+{
+    EditText txtUser, txtPass;
+    Switch saveSwitch;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtUser = findViewById(R.id.txtUsuario);
+        txtPass = findViewById(R.id.txtClave);
+        saveSwitch = findViewById(R.id.switchGuardar);
+        btnLogin = findViewById(R.id.btnAcceder);
+    }
+}
